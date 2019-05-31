5f422822198fdb2343dcc53267e16fbe8ada05b1
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-30 23:13:17 -0500

    minor styling

--- server/routes/name.router.js
+++ server/routes/name.router.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const pool = require('../modules/pool');
 // const axios = require('axios');
 require('dotenv').config();
 
 router.post('/', (req, res) => {
-    const queryText = 'INSERT INTO "';
+    const queryText = `INSERT INTO "words" (text) VALUES ('test text');`;
     pool.query(queryText)
     .then(() => res.sendStatus(201))
     .catch(() => res.sendStatus(500));
 });
+
+module.exports = router;
