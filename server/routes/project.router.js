fd6b3cd46d38185f2054b29bca93a7d18c7656e8
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-06-03 10:27:56 -0500

    project.router.js posts to server

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 router.post('/', (req, res) => {
     console.log('req.body', req.body.text)
-    const text = req.body.text;
-    const queryText = `INSTERT INTO "project" (user_id) VALUES $1. ; INSERT INTO "words" ("text") VALUES ($2);`;
-    pool.query(queryText, [text])
+    // const text = req.body.text;
+    const queryText = `INSERT INTO "project" ("user_id", "project_name") VALUES (1, 'test dog');`;
+    pool.query(queryText)
         .then(() => res.sendStatus(201))
         .catch(() => res.sendStatus(500));
 });
